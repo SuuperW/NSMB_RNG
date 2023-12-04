4ed38f7c79e30fc160aec802b81c5e30eec74d92
SuuperW <SuuperW@gmail.com>
2023-12-04 02:04:53 -0600

    some cleanup for imports, usings, and tabs

--- NSMB_RNG_WebApp/Controllers/SeedFindController.cs
+++ NSMB_RNG_WebApp/Controllers/SeedFindController.cs
@@ -1,36 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NSMB_RNG_WebApp.Models;
+using System;
 
 namespace NSMB_RNG_WebApp.Controllers
 {
-    [ApiController]
-    [Route($"{Config.ApiRotuePrefix}seedfindingresults")]
-    public class SeedFindController : ControllerBase
-    {
-        private readonly ILogger<SeedFindController> _logger;
+	[ApiController]
+	[Route($"{Config.ApiRotuePrefix}seedfindingresults")]
+	public class SeedFindController : ControllerBase
+	{
+		private readonly ILogger<SeedFindController> _logger;
 
-        public SeedFindController(ILogger<SeedFindController> logger)
-        {
-            _logger = logger;
-        }
+		public SeedFindController(ILogger<SeedFindController> logger)
+		{
+			_logger = logger;
+		}
 
-        // TODO: I get a 500 internal server error (AmbiguousMatchException) if the post request's content doesn't match the model.
-        // Obviously, such a request should be a 400 Bad Request instead.
-        [HttpPost]
-        public void LogSeedFindResults(SeedFindResultRaw result)
-        {
-            // TODO: Actually do stuff.
-            Console.WriteLine("Received result data.");
-            if (result == null)
-                Console.WriteLine("Data is null");
-            else if (result.macInput == null)
-                Console.WriteLine("macInput is null");
-            else
-            {
-                Console.WriteLine(result.macInput);
-                Console.WriteLine(result.consoleType);
-                Console.WriteLine(result.seed);
-            }
-        }
-    }
+		// TODO: I get a 500 internal server error (AmbiguousMatchException) if the post request's content doesn't match the model.
+		// Obviously, such a request should be a 400 Bad Request instead.
+		[HttpPost]
+		public void LogSeedFindResults(SeedFindResultRaw result)
+		{
+			// TODO: Actually do stuff.
+			Console.WriteLine("Received result data.");
+			if (result == null)
+				Console.WriteLine("Data is null");
+			else if (result.macInput == null)
+				Console.WriteLine("macInput is null");
+			else
+			{
+				Console.WriteLine(result.macInput);
+				Console.WriteLine(result.consoleType);
+				Console.WriteLine(result.seed);
+			}
+		}
+	}
 }
