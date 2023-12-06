2574e6c576dfa773ae2abf6bda84b52bc8d840e6
SuuperW <SuuperW@gmail.com>
2023-12-06 02:11:34 -0600

    add some tests

---
+++ NSMB_RNG_WebApp/ClientApp/src/app/seed-calculator.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+
+import { assert } from '../test/assert';
+import { SeedCalculator } from './seed-calculator';
+
+describe('SeedCalculator', () => {
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+	});
+
+	it('compute example seed', async () => {
+		let seedCalc = new SeedCalculator('40:f4:07:f7:d4:21', new Date(2023, 11, 4, 1, 2, 3));
+		seedCalc.timer0 = 400;
+		seedCalc.vCount = 123;
+		seedCalc.vFrame = 5;
+		assert(await seedCalc.getSeed() == 0xf2b9aa20);
+	});
+});
