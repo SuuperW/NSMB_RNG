614faea66c860023ca79c92bc0508eab9d08f14e
SuuperW <SuuperW@gmail.com>
2024-01-16 20:36:02 -0600

    Move code for creating additional workers from the worker into WorkerWrapper. This avoids the circular dependency issue.

--- NSMB_RNG_WebApp/ClientApp/src/app/app.worker.ts
+++ NSMB_RNG_WebApp/ClientApp/src/app/app.worker.ts
@@ -1,10 +1,7 @@
 /// <reference lib="webworker" />
 
 import { findRow2Matches } from "./functions/tiles";
-import { RngParams, SearchParams, searchForSeeds, searchForTime } from './functions/rng-params-search';
-// This import causes a warning about circular dependency. Apparently it's from webpack and could lead to improper caching by browsers...???
-// I don't understand how it works, and I'm going to ignore it because I cannot find a staisfactory solution.
-import { WorkerWrapper } from './worker-wrapper';
+import { RngParams, searchForSeeds, searchForTime } from './functions/rng-params-search';
 
 addEventListener('message', async (event: MessageEvent) => {
 	switch (event.data.func) {
@@ -19,55 +16,10 @@ addEventListener('message', async (event: MessageEvent) => {
 			return;
 		}
 		case searchForSeeds.workerName: {
-			if (event.data.options.minVCount > 0 || event.data.options.maxVCount < 263) {
-				// We have restricted search params, probably not very helpful to create workers for a relatively short operation.
-				// Also we don't want to recursively create 4, 16, 64, etc. workers.
-				postMessage({
-					data: searchForSeeds(event.data.seeds, event.data.options),
-					id: event.data.id,
-				});
-			} else {
-				// Use 4 workers
-				let options: SearchParams[] = [];
-				options.push(new SearchParams({
-					...event.data.options,
-					minTimer0: event.data.options.minTimer0,
-					minVCount: event.data.options.minVCount,
-					maxTimer0: (event.data.options.maxTimer0 + event.data.options.minTimer0) >>> 1,
-					maxVCount: (event.data.options.maxVCount + event.data.options.minVCount) >>> 1,
-				}));
-				options.push(new SearchParams({
-					...event.data.options,
-					minTimer0: options[0].maxTimer0 + 1,
-					minVCount: event.data.options.minVCount,
-					maxTimer0: event.data.options.maxTimer0,
-					maxVCount: (event.data.options.minVCount + event.data.options.minVCount) >>> 1,
-				}));
-				options.push(new SearchParams({
-					...event.data.options,
-					minTimer0: event.data.options.minTimer0,
-					minVCount: options[0].maxVCount + 1,
-					maxTimer0: (event.data.options.maxTimer0 + event.data.options.minTimer0) >>> 1,
-					maxVCount: event.data.options.maxVCount,
-				}));
-				options.push(new SearchParams({
-					...event.data.options,
-					minTimer0: options[0].maxTimer0 + 1,
-					minVCount: options[0].maxVCount + 1,
-					maxTimer0: event.data.options.maxTimer0,
-					maxVCount: event.data.options.maxVCount,
-				}));
-
-				let promises: Promise<RngParams[]>[] = [];
-				for (let i = 0; i < 3; i++) {
-					promises.push(new WorkerWrapper().searchForSeeds(event.data.seeds, options[i]));
-				}
-				let result = searchForSeeds(event.data.seeds, options[3]);
-				for (let p of promises) {
-					result.push(...(await p));
-				}
-				postMessage({ data: result, id: event.data.id });
-			}
+			postMessage({
+				data: searchForSeeds(event.data.seeds, event.data.options),
+				id: event.data.id,
+			});
 			return;
 		}
 		case searchForTime.workerName: {
@@ -76,63 +28,10 @@ addEventListener('message', async (event: MessageEvent) => {
 			let params: RngParams = event.data.params;
 			params.datetime = new Date(params.datetime);
 
-			if (event.data.minYear) {
-				postMessage({
-					data: searchForTime(seeds, params, event.data.minYear, event.data.maxYear),
-					id: event.data.id,
-				});
-			} else {
-				// Initialize
-				let cYear = 2000;
-				let maxYear = params.is3DS ? 2050 : 2100;
-				let initialSeconds = params.datetime.getSeconds();
-				let workers: WorkerWrapper[] = [];
-				for (let i = 0; i < Math.max(1, navigator.hardwareConcurrency - 1); i++) {
-					workers.push(new WorkerWrapper());
-				}
-
-				// Set up promise that we'll resolve once work is complete
-				let foundTime: Date | null = null;
-				let resolvePromise: (value: Date | null) => void;
-				let promise = new Promise<Date | null>(function (resolve, _) {
-					resolvePromise = resolve;
-				});
-
-				// Function to start checking next year after searching current year finishes
-				let attachThen = (p: Promise<Date | null>, workerId: number) => {
-					p.then((dt) => {
-						if (foundTime) return; // another worker found a time already
-						if (dt) { // this worker found a time
-							foundTime = dt;
-							resolvePromise(dt);
-							return;
-						}
-						// If we have gone through all years, increment seconds and start over.
-						if (cYear == maxYear) {
-							cYear = 2000;
-							params.datetime.setSeconds(params.datetime.getSeconds() + 1);
-							if (params.datetime.getSeconds() == initialSeconds) {
-								// This will probably never happen
-								foundTime = new Date(); // set this so other workers will stop
-								resolvePromise(null);
-								return;
-							}
-						}
-						attachThen(workers[workerId].searchForTime(event.data.seeds, params, cYear, ++cYear), workerId);
-					});
-				}
-
-				// Start all workers
-				for (let i = 0; i < navigator.hardwareConcurrency - 1; i++) {
-					attachThen(workers[i].searchForTime(event.data.seeds, params, cYear, ++cYear), i);
-				}
-
-				postMessage({
-					data: await promise,
-					id: event.data.id,
-				});
-			}
-
+			postMessage({
+				data: searchForTime(seeds, params, event.data.minYear, event.data.maxYear),
+				id: event.data.id,
+			});
 			return;
 		}
 		default: {
--- NSMB_RNG_WebApp/ClientApp/src/app/worker-wrapper.ts
+++ NSMB_RNG_WebApp/ClientApp/src/app/worker-wrapper.ts
@@ -1,6 +1,6 @@
-import * as rng from "./functions/rng";
 import { findRow2Matches } from "./functions/tiles";
 import * as paramSearch from "./functions/rng-params-search";
+import { RngParams, SearchParams } from "./functions/rng-params-search";
 
 let workerSupported: boolean = !!Worker;
 type dict = { [key: string]: any };
@@ -59,21 +59,116 @@ export class WorkerWrapper {
 	}
 
 	async searchForSeeds(seeds: number[], options: paramSearch.SearchParams) {
-		if (this.worker)
-			return await this.call(paramSearch.searchForSeeds.workerName, { seeds: seeds, options: options }) as paramSearch.RngParams[];
+		if (this.worker) {
+			// Use 4 workers if the search params aren't constrained.
+			if (options.minVCount == 0 && options.maxVCount == 263) {
+				let subOptions: SearchParams[] = [];
+				subOptions.push(new SearchParams({
+					...options,
+					minTimer0: options.minTimer0,
+					minVCount: options.minVCount,
+					maxTimer0: (options.maxTimer0 + options.minTimer0) >>> 1,
+					maxVCount: (options.maxVCount + options.minVCount) >>> 1,
+				}));
+				subOptions.push(new SearchParams({
+					...options,
+					minTimer0: subOptions[0].maxTimer0 + 1,
+					minVCount: options.minVCount,
+					maxTimer0: options.maxTimer0,
+					maxVCount: (options.minVCount + options.minVCount) >>> 1,
+				}));
+				subOptions.push(new SearchParams({
+					...options,
+					minTimer0: options.minTimer0,
+					minVCount: subOptions[0].maxVCount + 1,
+					maxTimer0: (options.maxTimer0 + options.minTimer0) >>> 1,
+					maxVCount: options.maxVCount,
+				}));
+				subOptions.push(new SearchParams({
+					...options,
+					minTimer0: subOptions[0].maxTimer0 + 1,
+					minVCount: subOptions[0].maxVCount + 1,
+					maxTimer0: options.maxTimer0,
+					maxVCount: options.maxVCount,
+				}));
+
+				let promises: Promise<RngParams[]>[] = [];
+				promises.push(this.searchForSeeds(seeds, subOptions[0]));
+				for (let i = 1; i < subOptions.length; i++) {
+					promises.push(new WorkerWrapper().searchForSeeds(seeds, subOptions[i]));
+				}
+				let result = [];
+				for (let p of promises) {
+					result.push(...(await p));
+				}
+				return result;
+			} else {
+				return await this.call(paramSearch.searchForSeeds.workerName, { seeds: seeds, options: options }) as paramSearch.RngParams[];
+			}
+		}
 		else
 			return paramSearch.searchForSeeds(seeds, options);
 	}
 
 	async searchForTime(seeds: number[], params: paramSearch.RngParams, minYear?: number, maxYear?: number) {
-		if (this.worker)
-			return await this.call(paramSearch.searchForTime.workerName, {
-				seeds: seeds,
-				params: params,
-				minYear: minYear,
-				maxYear: maxYear,
-			}) as Date | null;
-		else
+		if (this.worker) {
+			if (!minYear) minYear = 2000;
+			if (!maxYear) maxYear = params.is3DS ? 2050 : 2100;
+			if (minYear + 1 != maxYear) {
+				// Initialize
+				let foundTime: Date | null = null;
+				let cYear = 2000;
+				let initialSeconds = params.datetime.getSeconds();
+				let workers: WorkerWrapper[] = [this];
+				for (let i = 1; i < Math.max(1, navigator.hardwareConcurrency - 1); i++) {
+					workers.push(new WorkerWrapper());
+				}
+
+				// Set up promise that we'll resolve once work is complete
+				let resolvePromise: (value: Date | null) => void;
+				let promise = new Promise<Date | null>(function (resolve, _) {
+					resolvePromise = resolve;
+				});
+
+				// Function to start checking next year after searching current year finishes
+				let attachThen = (p: Promise<Date | null>, workerId: number) => {
+					p.then((dt) => {
+						if (foundTime) return; // another worker found a time already
+						if (dt) { // this worker found a time
+							foundTime = dt;
+							resolvePromise(dt);
+							return;
+						}
+						// If we have gone through all years, increment seconds and start over.
+						if (cYear == maxYear) {
+							cYear = 2000;
+							params.datetime.setSeconds(params.datetime.getSeconds() + 1);
+							if (params.datetime.getSeconds() == initialSeconds) {
+								// This will probably never happen
+								foundTime = new Date(); // set this so other workers will stop
+								resolvePromise(null);
+								return;
+							}
+						}
+						attachThen(workers[workerId].searchForTime(seeds, params, cYear, ++cYear), workerId);
+					});
+				}
+
+				// Start all workers
+				for (let i = 0; i < navigator.hardwareConcurrency - 1; i++) {
+					attachThen(workers[i].searchForTime(seeds, params, cYear, ++cYear), i);
+				}
+
+				return await promise;
+			} else {
+				return await this.call(paramSearch.searchForTime.workerName, {
+					seeds: seeds,
+					params: params,
+					minYear: minYear,
+					maxYear: maxYear,
+				}) as Date | null;
+			}
+		} else
 			return paramSearch.searchForTime(new Set(seeds), params, 2000, params.is3DS ? 2050 : 2100);
 	}
 }
