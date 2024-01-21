3787425e3ef4d43def217be00168b95e5b03cfac
SuuperW <SuuperW@gmail.com>
2024-01-21 00:30:34 -0600

    Use caching in the pipeline, and use the Azure CLI tool directly instead of specialized pipeline tasks so that deployment is more transparent.

---
+++ NSMB_RNG_WebApp/azDeploy.bat
@@ -0,0 +1,6 @@
+﻿@ECHO off
+
+echo "Running infrastructure deployment"
+call az deployment sub create --template-file NSMB_RNG_WebApp/main.bicep -l eastus -o none
+echo "Running software deployment"
+call az webapp deployment source config-zip --src pub.zip -n NSMB-RNG -g NSMB_RNG
